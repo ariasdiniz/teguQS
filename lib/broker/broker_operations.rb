acc90f710e9e99f764712a4de21eb3c4118942cf
Aria Diniz <aria.diniz.dev@gmail.com>
2022-07-31 19:47:11 -0300

    Fixing bug on broker_operations after lib update

--- lib/broker/broker_operations.rb
+++ lib/broker/broker_operations.rb
@@ -3,10 +3,9 @@ require 'singly_linked_queue'
 module BrokerOperations
   def add_message_to_queue(message, topic)
     if self.queues[topic].nil?
-      self.queues[topic] = SinglyLinkedQueue::LinkedQueue.new(message)
-    else
-      self.queues[topic].put(message)
+      self.queues[topic] = SinglyLinkedQueue::LinkedQueue.new
     end
+    self.queues[topic].put(message)
   end
 
   def send_to_listener(topic)
