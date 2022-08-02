03b7a9019555bc7f07ee3b9449caecba5961ccce
Aria Diniz <aria.diniz.dev@gmail.com>
2022-08-01 21:22:52 -0300

    Implementing factory for the producer and consumer

--- lib/tegu_qs.rb
+++ lib/tegu_qs.rb
@@ -1,7 +1,17 @@
 require_relative 'broker/broker_starter'
+require_relative 'consumer/consumer'
+require_relative 'producer/producer'
 
 module TeguQS
   def self.start_broker
     BrokerStarter.run!
+  end
+
+  def self.consumer(topic, uri = 'http://localhost', port = 4566)
+    Consumer.new(topic, uri, port)
+  end
+
+  def self.producer(topic, uri = 'http://localhost', port = 4566)
+    Producer.new(topic, port, uri)
   end
 end
